9d3b770fd2b3b67e7d29d68ccb3b3ffdd8ce5734
ANKI <ANKI@intechS1.fr>
2018-05-12 11:43:51 +0200

    Problème de chemin sur le serveur

--- WORLD_LADDER2/serveur.js
+++ WORLD_LADDER2/serveur.js
@@ -8,33 +8,30 @@
 
 var http = require("http");
 var url = require("url");
-var mon_serveur;
-var port;
 var querystring = require("querystring");
-var fs = require("fs");
 
 //-------------------------------------------------------------------------
 // DECLARATION DES DIFFERENTS MODULES CORRESPONDANT A CHAQUE ACTION
 //-------------------------------------------------------------------------
 
-var req_commencer = require("./js/req_commencer.js");
+var req_commencer = require('./js/req_commencer.js');
 var req_confirmation_inscription = require("./js/req_confirmation_inscription.js");
-var req_defie = require("./js/req_defie.js");
-var req_dice = require("./js/req_dice.js");
-var req_gagner = require("./js/req_gagner.js");
-var req_identifier = require("./js/req_identifier.js");
-var req_modele_confirmation_inscription = require("./js/req_modele_confirmation_inscription.js");
-var req_modele_formulaire_inscription = require("./js/req_modele_formulaire_inscription.js");
-var req_non = require("./js/req_non.js");
-var req_oui = require("./js/req_oui.js");
-var req_oui2 = require("./js/req_oui2.js");
-var req_perdu = require("./js/req_perdu.js");
-var req_reponse_defie = require("./js/req_reponse_defie.js");
+var req_defie = require('./js/req_defie.js');
+var req_dice = require('./js/req_dice.js');
+var req_gagner = require('./js/req_gagner.js');
+var req_identifier = require('./js/req_identifier.js');
+var req_modele_confirmation_inscription = require('./js/req_modele_confirmation_inscription.js');
+var req_modele_formulaire_inscription = require('./js/req_modele_formulaire_inscription.js');
+var req_non = require('./js/req_non.js');
+var req_oui = require('./js/req_oui.js');
+var req_oui2 = require('./js/req_oui2.js');
+var req_perdu = require('./js/req_perdu.js');
+var req_reponse_defie = require('./js/req_reponse_defie.js');
 
 
 
-var req_statique = require("./js/req_statique.js");
-var req_erreur = require("./js/req_erreur.js");
+var req_statique = require('./js/req_statique.js');
+var req_erreur = require('./js/req_erreur.js');
 
 
 //-------------------------------------------------------------------------
@@ -42,6 +39,7 @@ var req_erreur = require("./js/req_erreur.js");
 //-------------------------------------------------------------------------
 
 var traite_requete = function (req, res) {
+
 	var ressource;
 	var requete;
 	var pathname;;
@@ -56,45 +54,45 @@ var traite_requete = function (req, res) {
 	try {
 		switch (pathname) {
 			case '/':
-			case "./js/req_commencer.js":
+			case './js/req_commencer':
 				req_commencer(req, res, query);
 				break;
-			case "./js/req_confirmation_inscription.js":
+			case './js/req_confirmation_inscription':
 				req_confirmation_inscription(req, res, query);
 				break;
-			case "./js/req_defie.js":
+			case './js/req_defie':
 				req_defie(req, res, query);
 				break;
-			case "./js/req_dice.js":
+			case './js/req_dice':
 				req_dice(req, res, query);
 				break;
-			case "./js/req_gagner.js":
+			case './js/req_gagner':
 				req_gagner(req, res, query);
 				break;
-			case "./js/req_identifier.js":
+			case './js/req_identifier':
 				req_identifier(req, res, query);
 				break;
-			case "./js/req_modele_confirmation_inscription.js":
+			case './js/req_modele_confirmation_inscription':
 				req_modele_confirmation_inscription(req, res, query);
 				break;
-			case "./js/req_modele_formulaire_inscription.js":
+			case './js/req_modele_formulaire_inscription':
 				req_modele_formulaire_inscription(req, res, query);
 				break;
-			case "./js/req_non.js":
+			case './js/req_non':
 				req_non(req, res, query);
 				break;
-			case "./js/oui.js":
+			case './js/oui':
 				req_oui(req, res, query);
 				break;
 
 
-			case './js/req_oui2.js':
+			case './js/req_oui2':
 				req_oui2(req, res, query);
 				break;
-			case './js/req_perdu.js':
+			case './js/req_perdu':
 				req_perdu(req, res, query);
 				break;
-			case './js/req_reponse_defie.js':
+			case './js/req_reponse_defie':
 				req_reponse_defie(req, res, query);
 				break;
 			default:
