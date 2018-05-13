37caea675c1a1791aa3696059320512a973f8f6d
ANKI <ANKI@intechS1.fr>
2018-05-13 22:10:53 +0200

    Salon - OK. Rajout des joueurs en temps réel ainsi que de la possibilité de les défiers. -En cours- Elaboration de la requête défié.

--- WORLD_LADDER2/js/req_identifier.js
+++ WORLD_LADDER2/js/req_identifier.js
@@ -14,6 +14,8 @@ var trait = function (req, res, query) {
     var listeMembres;
     var i;
     var trouve;
+	var test = false;
+	
 
 
     // ON LIT LES COMPTES EXISTANTS
@@ -49,19 +51,67 @@ var trait = function (req, res, query) {
     } else if (trouve === true) {
         // SI IDENTIFICATION OK, ON ENVOIE PAGE ACCUEIL MEMBRE
 
-        page = fs.readFileSync('./html/res_salon.html', 'UTF-8');
+		page = fs.readFileSync('./html/res_salon.html', 'UTF-8');
+		
 
+//==============================================================================================//
+//PREPARATION DE LA LISTE DE MEMBRE CONNECTE EN TEMPS REEL//
 
+		var membre_co_salon;
+		var contenu_fichier2;
+		var liste_fichier2;
+		var liste_temps_reel;
+		var liste;
+		
+	
+//========================== Récupération des informatons du lobby =============================\\
 
+		contenu_fichier2 = fs.readFileSync("./json/salon.json", 'utf-8');
+		liste_temps_reel = JSON.parse(contenu_fichier2);
 
+//========================== Premier cas, le joueur est déjà dans le salon ======================\\
 
+		for(i = 0; i < liste_temps_reel.length; i++){
+			if(liste_temps_reel[i].compte === query.compte){
+				test = true;
+				liste_temps_reel[i].etat = "connecté";
+				contenu_fichier2 = JSON.stringify(liste_temps_reel);
+				fs.writeFileSync("./json/salon.json", contenu_fichier2, 'utf-8');
+			}
+		} 
 
+//========================= Second cas, le joueur n'y était pas ==================================\\
+		if(test === false){
+			membre_co_salon = {};
+			membre_co_salon.compte = query.compte;
+			membre_co_salon.etat = "connecté";
+			membre_co_salon.libre = "oui";
+			liste_temps_reel = [];
+			liste_temps_reel.push(membre_co_salon);
 
-    }
+			contenu_fichier2 = JSON.stringify(liste_temps_reel);
+			fs.writeFileSync("./json/salon.json", contenu_fichier2, 'utf-8');
+		}
+
+		page = fs.readFileSync('./html/res_salon.html', 'UTF-8');
+
+		liste= "";
+		for (i = 0; i < liste_temps_reel.length; i++) {
+			if (liste_temps_reel[i].compte !== query.compte && liste_temps_reel[i].etat === "connecté") {
+				liste += "<form action='./js/req_reponse_defie.js' method='GET'><input type ='hidden' name='compte' value='"+ query.compte +"'><input type ='hidden' name ='libre' value='"+ liste_temps_reel[i].compte +"'><button class='button1' name='action' value=''>" + liste_temps_reel[i].compte + "</button></form>";
+			}
+					
+
+		}
+	
+	}
+		marqueurs = {};
+        marqueurs.compte = query.compte;
+        marqueurs.mdp = query.mdp;
+		marqueurs.joueurs = liste
+
+        page = page.supplant(marqueurs);
     
-
-
-
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(page);
     res.end();
